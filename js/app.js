9f45fc70a21dd9d4771daf0d7a37523308124dfc
edmundarvin1 <edmundarvin1@gmail.com>
2016-02-07 19:44:10 +0800

    with sound

--- js/app.js
+++ js/app.js
@@ -5,6 +5,29 @@ $(document).ready(function() {
 	})
 	.mouseleave(function(){
 		$('.ryu-still').show();
+		$('.ryu-ready').hide();	
+	})
+	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show().finish()
+		.animate(
+			{'left': '1220px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			});
+	})
+	.mouseup(function() {
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+
 	});
 });
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
